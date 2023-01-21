b908f7e18e27a0606f950caaf6dca23f2954afe4
Nasenboi <c.boendgen@gmail.com>
2023-01-21 17:17:25 +0100

    added possible sr conversion

--- AudioDataPackage/AudioToInputData/AudioToInputData.py
+++ AudioDataPackage/AudioToInputData/AudioToInputData.py
@@ -1,16 +1,31 @@
 #import os
 import librosa
 import numpy as np
+import scipy.signal as sig
+
+def butter_lowpass(cutoff, fs, order=10):
+    return sig.butter(order, cutoff, fs=fs, btype='low', analog=False)
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = sig.lfilter(b, a, data, axis = 0)
+    return y
 
 
-
-def audioToInputData(samples, sampleRate, blockSize, hopSize, specBlockSize, specHopSize):
+def audioToInputData(samples, sampleRateSample, blockSize, hopSize, specBlockSize, specHopSize, sampleRateSpec = None):
     try:
+        if(sampleRateSpec):
+            #Apply a Low-Pass Filter and resample, idk if this is nescesarry, we vould just load() librosa with a certain samplerate,
+            #but idk if its LP to get rid of aliasing
+            samples = butter_lowpass_filter(data=samples, cutoff=7000, fs=sampleRateSample)
+            samples = librosa.resample(y=samples, orig_sr=sampleRateSample, target_sr=sampleRateSpec, axis = 0)
+
         #Calculate initial variables
         N = len(samples)
         if (N<blockSize):
             print("Error: Num samples is too short!")
             return []
+        
 
         #Add zeros until a certain size
         if(N-blockSize % hopSize != 0):
@@ -26,7 +41,6 @@ def audioToInputData(samples, sampleRate, blockSize, hopSize, specBlockSize, spe
         for frame in audioFrames:
             spec = librosa.stft(frame, n_fft=specBlockSize, hop_length=specHopSize)
             specList.append(spec)
-
 
         return specList    
     except:
--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-from random import sample
 import AudioDataPackage.AudioToInputData.AudioToInputData as atid
 import librosa
 import librosa.display
 import numpy as np
 import matplotlib.pyplot as plt
 
-path = r"C:\Users\Chr1s\Documents\Programmierungen\GitHubStuff\AiudioDataScience_Warnsystem-\AudioAugmentation\TrynError\BrumD.wav"
+path = r"F:\Labeled_Audio\166101-5-2-4.wav"
 
-samples, sampleRate = librosa.load(path)
+sampleRateSpec = 16000
+
+samples, sampleRate = librosa.load(path, sr=sampleRateSpec)
 
 #sampleList = [float(i) for i in range(20)]
 #sampleRate = 48000;
 #samples = np.array(sampleList)
 
-frames = atid.audioToInputData(samples, sampleRate, sampleRate, int(0.5*sampleRate), 2048, 512)
-
+frames = atid.audioToInputData(samples=samples, sampleRateSample=sampleRate, blockSize=16000, hopSize=8000, specBlockSize=2048, specHopSize=512)
 S = np.abs(frames[len(frames)-1])
 
 fig, ax = plt.subplots()
-img = librosa.display.specshow(librosa.amplitude_to_db(S, ref=np.max), y_axis='log', x_axis='time', ax=ax)
+img = librosa.display.specshow(librosa.amplitude_to_db(S, ref=np.max), y_axis='log', x_axis='time', ax=ax, sr = sampleRateSpec)
 ax.set_title('Power spectrogram')
 fig.colorbar(img, ax=ax, format="%+2.0f dB")
 
